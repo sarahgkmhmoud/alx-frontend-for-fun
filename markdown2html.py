5fe9f6243c493b9dac1993ba08be95f97fd2a7ad
Sarahgad <sarahgad91@gmail.com>
2024-05-05 06:12:21 +0300

    0. Add display flex

--- markdown2html.py
+++ markdown2html.py
@@ -20,13 +20,28 @@ if __name__ == '__main__':
 
     with open(sys.argv[1]) as read:
         with open(sys.argv[2], 'w') as html:
+            unordered_start = False
             for line in read:
+                # heading
                 length = len(line)
                 headings = line.lstrip('#')
                 heading_num = length - len(headings)
+                unordered = line.lstrip('-')
+                unordered_num = length - len(unordered)
                 if 1 <= heading_num <= 6:
                     line = '<h{}>'.format(
                         heading_num) + headings.strip() + '</h{}>\n'.format(
                         heading_num)
+                if unordered_num:
+                    if not unordered_start:
+                        html.write('<ul>\n')
+                        unordered_start = True
+                    line = '<li>' + unordered.strip() + '</li>\n'
+                if unordered_start and not unordered_num:
+                    html.write('</ul>\n')
+                    unordered_start = False
                 if length > 1:
                         html.write(line)
+            if unordered_start:
+                html.write('</ul>\n')
+    exit (0)
